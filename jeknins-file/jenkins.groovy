6ee7129d3c42dff7d1bae58b379d36a5911a4c8f
KHAN Adnan <adnan.khan@soprasteria.com>
2019-01-06 13:53:38 +0530

    groovy

---
+++ jeknins-file/jenkins.groovy
@@ -0,0 +1,20 @@
+pipeline
+{
+
+agent 'master'
+
+    stages
+    {   
+        stage('Build')
+        {
+            steps
+            {
+                echo "compiling code"
+                echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
+                sh 'mvn clean compile';
+            }
+        }
+
+    }   
+
+}
