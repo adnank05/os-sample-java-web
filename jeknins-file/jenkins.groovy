84f7dd440c038607fa3767df77103ad22448d0eb
KHAN Adnan <adnan.khan@soprasteria.com>
2019-01-06 14:03:00 +0530

    groovy

--- jeknins-file/jenkins.groovy
+++ jeknins-file/jenkins.groovy
@@ -3,8 +3,24 @@ pipeline
 
 agent any
 
+    tools
+    {
+        maven 'Maven-3.6.0'
+        jdk 'java-1.8'
+    }
+
     stages
     {   
+        stage('Initializing Tools')
+        {
+              steps
+                {
+                    sh '''
+                        echo "PATH = ${PATH}"
+                        echo "M2_HOME = ${M2_HOME}"
+                    '''
+                }
+        }
         stage('Build')
         {
             steps
